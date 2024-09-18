f01439083948a490ddded404d656220c39c87951
Zolookor2004 <zolookorzoloo@gmail.com>
2024-09-18 15:24:42 +0800

    env

--- db.ts
+++ db.ts
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGODB_URI = process.env.mongoDBConnection_uri || "";
+
+console.log(MONGODB_URI);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      `mongodb+srv://zolookorzoloo2004:VGno8yxaSMFTYnoo@team-1-db.sfhni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-    );
+    const conn = await mongoose.connect(MONGODB_URI);
     console.log(`MongoDB Connected: {conn.connection.host}`);
   } catch (error) {
     console.error(error);
