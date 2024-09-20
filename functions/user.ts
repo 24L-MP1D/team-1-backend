b7753d117fcdd1cbd964b3ca7d42daf4c9677eb0
Zolookor2004 <zolookorzoloo@gmail.com>
2024-09-20 17:45:25 +0800

    minor pull

--- functions/user.ts
+++ functions/user.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+var bcrypt = require("bcryptjs");
+
 export const createUser = (req: Request, res: Response) => {
+  const {
+    name,
+    username,
+    password
+  }: { name: string; username: string; password: string } = req.body;
+  console.log(name, username, password);
+
   try {
-    console.log("asd");
+    var salt = bcrypt.genSaltSync(Number(process.env.saltNumber));
+    const hashedPass = bcrypt.hashSync(password, salt);
+
+    return res.send("success");
   } catch (error) {
     console.error(error);
   }
